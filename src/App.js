d9c70df9dae5f6986fb5f21ce84c332e524a76e1
Joffrey <muzhyke1@gmail.com>
2021-06-21 16:44:20 +0300

    Reg/auth validation finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './css/index.css';
+import { Route } from 'react-router-dom';
 import RegistrationModuleBasic from './components/RegistrationModuleBasic';
 import RegistrationModuleEmailIndividual from './components/RegistrationModuleEmailIndividual';
-import { Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="wrapper">
       <div className="content">
+        <Route path="/" component={Home} exact />
         <Route path="/registration" component={RegistrationModuleBasic} exact />
         <Route path="/registration-step-2" component={RegistrationModuleEmailIndividual} exact />
       </div>
--- src/components/RegistrationModuleEmailIndividual.jsx
+++ src/components/RegistrationModuleEmailIndividual.jsx
@@ -9,6 +9,14 @@ import googleLogo from '../img/Google.png';
 const RegistrationModuleEmailIndividual = () => {
   const dispatch = useDispatch();
   const { sentNumber, sentEmail, password } = useSelector(({ registration }) => registration);
+  const inputErrors = [
+    'Слишком короткая запись',
+    'Поле не может быть пустым',
+    'Некорректный Email',
+    'Некорректный номер телефона',
+  ];
+  const contactEmailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const contactNumberRegExp = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
 
   const [name, setName] = React.useState();
   const [surname, setSurname] = React.useState();
@@ -16,9 +24,111 @@ const RegistrationModuleEmailIndividual = () => {
   const [email, setEmail] = React.useState(sentEmail);
   const [number, setNumber] = React.useState(sentNumber);
   const [promo, setPromo] = React.useState();
+  const [nameDirty, setNameDirty] = React.useState();
+  const [surnameDirty, setSurnameDirty] = React.useState();
+  const [birthDirty, setBirthDirty] = React.useState();
+  const [emailDirty, setEmailDirty] = React.useState();
+  const [numberDirty, setNumberDirty] = React.useState();
+  const [nameError, setNameError] = React.useState(inputErrors[1]);
+  const [surnameError, setSurnameError] = React.useState('Это поле не может быть пустым');
+  const [birthError, setBirthError] = React.useState('Это поле не может быть пустым');
+  const [emailError, setEmailError] = React.useState();
+  const [numberError, setNumberError] = React.useState();
+  const [formValid, setFormValid] = React.useState(false);
+
+  React.useEffect(() => {
+    if (nameError || surnameError || birthError || emailError || numberError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [nameError, surnameError, birthError, emailError, numberError]);
+
+  const nameHandler = (e) => {
+    setName(e.target.value);
+    if (e.target.value.length === 0) {
+      setNameError(inputErrors[1]);
+    } else if (e.target.value.length < 2) {
+      setNameError(inputErrors[0]);
+    } else {
+      setNameError('');
+    }
+  };
+
+  const surnameHandler = (e) => {
+    setSurname(e.target.value);
+    if (e.target.value.length === 0) {
+      setSurnameError(inputErrors[1]);
+    } else if (e.target.value.length < 2) {
+      setSurnameError(inputErrors[0]);
+    } else {
+      setSurnameError('');
+    }
+  };
+
+  const birthHandler = (e) => {
+    setBirth(e.target.value);
+    if (e.target.value.length === 0) {
+      setBirthError(inputErrors[1]);
+    } else {
+      setBirthError('');
+    }
+  };
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    if (e.target.value.length === 0) {
+      setEmailError(inputErrors[1]);
+    } else if (!contactEmailRegExp.test(String(e.target.value))) {
+      setEmailError(inputErrors[2]);
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const numberHandler = (e) => {
+    setNumber(e.target.value);
+    if (e.target.value.length === 0) {
+      setNumberError(inputErrors[1]);
+    } else if (!contactNumberRegExp.test(String(e.target.value))) {
+      setNumberError(inputErrors[3]);
+    } else {
+      setNumberError('');
+    }
+  };
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'name':
+        setNameDirty(true);
+        break;
+      case 'surname':
+        setSurnameDirty(true);
+        break;
+      case 'birth':
+        setBirthDirty(true);
+        break;
+      case 'email':
+        setEmailDirty(true);
+        break;
+      case 'number':
+        setNumberDirty(true);
+        break;
+    }
+  };
 
   const onClickSubmit = () => {
-    password ? alert('Все ок!') : alert('Заполните первую форму!');
+    if (email.length === 0 || number.length === 0) {
+      if (email.length === 0) {
+        setEmailDirty(true);
+        setEmailError(inputErrors[1]);
+      } else if (number.length === 0) {
+        setNumberDirty(true);
+        setNumberError(inputErrors[1]);
+      }
+    } else {
+      alert('Успешно!');
+    }
   };
   return (
     <div className="reg-content">
@@ -44,66 +154,86 @@ const RegistrationModuleEmailIndividual = () => {
                 Имя
               </label>
               <input
+                name="name"
                 id="name"
                 type="text"
                 placeholder="..."
                 className="reg-form-contact-input"
                 value={name}
                 onChange={(e) => {
-                  setName(e.target.value);
+                  nameHandler(e);
                 }}
+                onBlur={(e) => blurHandler(e)}
               />
+              {nameDirty && nameError && (
+                <label className="reg-form-text-label-l__alert">{nameError}</label>
+              )}
               <label htmlFor="surname" className="reg-form-text-label-l">
                 Фамилия
               </label>
               <input
+                name="surname"
                 id="surname"
                 type="text"
                 placeholder="..."
                 className="reg-form-contact-input"
                 value={surname}
-                onChange={(e) => {
-                  setSurname(e.target.value);
-                }}
+                onChange={(e) => surnameHandler(e)}
+                onBlur={(e) => blurHandler(e)}
               />
+              {surnameDirty && surnameError && (
+                <label className="reg-form-text-label-l__alert">{surnameError}</label>
+              )}
               <label htmlFor="birth" className="reg-form-text-label-l">
                 Дата рождения
               </label>
               <input
+                name="birth"
                 id="birth"
                 type="date"
                 className="reg-form-contact-input"
                 value={birth}
-                onChange={(e) => {
-                  setBirth(e.target.value);
-                }}
+                onChange={(e) => birthHandler(e)}
+                onBlur={(e) => blurHandler(e)}
               />
+              {birthDirty && birthError && (
+                <label className="reg-form-text-label-l__alert">{birthError}</label>
+              )}
               <label htmlFor="email" className="reg-form-text-label-l">
                 Email
               </label>
               <input
+                name="email"
                 id="email"
                 type="text"
                 className="reg-form-contact-input"
                 value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
+                onChange={(e) => emailHandler(e)}
+                onBlur={(e) => blurHandler(e)}
               />
+              {emailDirty && emailError && (
+                <label className="reg-form-text-label-l__alert">{emailError}</label>
+              )}
               <label htmlFor="number" className="reg-form-text-label-l">
-                Номер телефона
+                Номер телефона (с кодом страны)
               </label>
               <input
+                name="number"
                 id="number"
                 type="text"
                 className="reg-form-contact-input"
                 value={number}
-                onChange={(e) => setNumber(e.target.value)}
+                onChange={(e) => numberHandler(e)}
+                onBlur={(e) => blurHandler(e)}
               />
+              {numberDirty && numberError && (
+                <label className="reg-form-text-label-l__alert">{numberError}</label>
+              )}
               <label htmlFor="promo" className="reg-form-text-label-l">
                 Промокод
               </label>
               <input
+                name="promo"
                 id="promo"
                 type="text"
                 className="reg-form-contact-input"
@@ -117,6 +247,7 @@ const RegistrationModuleEmailIndividual = () => {
                 value="Зарегистрироваться"
                 className="reg-form-submit-button"
                 onClick={onClickSubmit}
+                disabled={!formValid}
               />
             </form>
           </div>
