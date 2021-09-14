6f8a93d122b536fef8ffa43c44f004bc96d133b1
mikkiklain <mikkiklain@gmail.com>
2021-09-14 22:00:47 +0300

    last update

--- src/components/MainPage/Partners.jsx
+++ src/components/MainPage/Partners.jsx
@@ -22,6 +22,7 @@ const Partners = () => {
       <div className="partners-content">
         <p className="partners-main-title">С нами сотрудничают</p>
         <div className="partners-slider-container">
+        <div id="swiper_comp">
           <Swiper
             spaceBetween={50}
             slidesPerView={4}
@@ -50,6 +51,7 @@ const Partners = () => {
                 </SwiperSlide>
               ))}
           </Swiper>
+        </div>
           {/* мобилка */}
           <div id="swiper_mobile">
             <Swiper
@@ -78,8 +80,8 @@ const Partners = () => {
             </Swiper>
           </div>
 
-                    {/* планшеты */}
-                    <div id="global_ipad" style={{width:'100%'}}>
+                    {/* планшеты 768 */}
+            <div id="global_ipad" style={{width:'100%'}}>
             <Swiper
               style={{
                 display: "flex",
@@ -88,6 +90,32 @@ const Partners = () => {
               }}
               spaceBetween={0}
               slidesPerView={3}
+              pagination={{ clickable: true }}
+              id="swiper_mobile"
+            >
+              {partners &&
+                partners.map((item, index) => (
+                  <SwiperSlide key={index} style={{width:'25%'}}>
+                    <a href={item.link_partner} target="_blank">
+                      <img
+                        src={`https://razdelisdrugim.by${item.image_partner}`}
+                      />
+                    </a>
+                  </SwiperSlide>
+                ))}
+            </Swiper>
+          </div>
+
+                              {/* планшеты 1024 */}
+            <div id="global_ipad_1024" style={{width:'100%'}}>
+            <Swiper
+              style={{
+                display: "flex",
+                width: "1024px",
+                justifyContent: "center",
+              }}
+              spaceBetween={20}
+              slidesPerView={4}
               pagination={{ clickable: true }}
               id="swiper_mobile"
             >
